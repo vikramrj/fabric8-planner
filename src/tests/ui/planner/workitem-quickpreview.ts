da1b7f2d230e8f3ee7ac93ff586240d2cd0dd52e
Ibrahim Jarif <jarifibrahim@gmail.com>
2018-03-06 14:31:53 +0530

    fix(tests): Run only smoke tests

--- src/tests/ui/planner/workitem-quickpreview.ts
+++ src/tests/ui/planner/workitem-quickpreview.ts
@@ -113,10 +113,6 @@ export class WorkItemQuickPreview extends ui.BaseElement {
     await this.assigneeDropdown.clickWhenReady();
     await this.assigneeDropdown.select(assignee);
     await this.assigneeDropdownCloseButton.clickWhenReady();
-
-    // Wait for notification to appear and disappear
-    await this.notificationToast.ready();
-    await this.notificationToast.untilHidden();
     await this.loadingAnimation.untilCount(0);
   }
 
@@ -126,9 +122,6 @@ export class WorkItemQuickPreview extends ui.BaseElement {
     await this.areaDropdown.clickWhenReady();
     await this.areaDropdown.select(areaTitle);
     await this.areaSaveButton.clickWhenReady();
-    // Wait for notification to appear and disappear
-    await this.notificationToast.ready();
-    await this.notificationToast.untilHidden();
     await this.loadingAnimation.untilCount(0);
   }
 
@@ -138,10 +131,6 @@ export class WorkItemQuickPreview extends ui.BaseElement {
     await this.iterationDropdown.clickWhenReady();
     await this.iterationDropdown.select(iterationTitle);
     await this.iterationSaveButton.clickWhenReady();
-    // Wait for notification to appear and disappear
-    await this.notificationToast.ready();
-    await this.notificationToast.untilHidden();
-    await this.loadingAnimation.untilCount(0);
   }
 
   private async addComment(comment: string) {
@@ -266,8 +255,6 @@ export class WorkItemQuickPreview extends ui.BaseElement {
     }
     await this.titleInput.enterText(title);
     await this.titleSaveButton.clickWhenReady();
-    await this.notificationToast.ready();
-    await this.notificationToast.untilHidden();
     await this.titleSaveButton.untilHidden();
   }
 
@@ -277,10 +264,9 @@ export class WorkItemQuickPreview extends ui.BaseElement {
       await this.descriptionTextarea.clear();
     }
     await this.descriptionTextarea.enterText(description);
+    await this.descriptionSaveButton.scrollIntoView();
     await this.descriptionSaveButton.clickWhenReady();
     await this.descriptionSaveButton.untilHidden();
-    await this.notificationToast.ready();
-    await this.notificationToast.untilCount(0);
   }
 
   async removeAssignee(assignee: string) {
