8f1bc0b2dbca79528d71257384a5d553c11c65eb
Vikram Raj <vikramraj1991@gmail.com>
2018-07-09 23:06:23 +0530

    fix(board):add drag and drop

--- src/app/components_ngrx/planner-board-column/planner-board-column.component.ts
+++ src/app/components_ngrx/planner-board-column/planner-board-column.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input } from '@angular/core';
-import { WorkItemUI } from './../../models/work-item';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { DragulaService } from 'ng2-dragula';
 
 @Component({
   selector: 'f8-planner-column',
@@ -7,6 +7,40 @@ import { WorkItemUI } from './../../models/work-item';
   styleUrls: ['./planner-board-column.component.less']
 })
 
-export class PlannerBoardColumnComponent {
-  @Input() workItems: WorkItemUI[];
+export class PlannerBoardColumnComponent implements OnDestroy, OnInit {
+  @Input() column;
+
+  private eventListeners: any[] = [];
+
+  constructor(private dragulaService: DragulaService) {
+  }
+
+  ngOnInit() {
+    this.eventListeners.push(
+      this.dragulaService.drop.subscribe((value) => {
+        this.onDrop(value.slice(1));
+      })
+    );
+  }
+
+  ngOnDestroy() {
+    this.eventListeners.forEach(subscriber => subscriber.unsubscribe());
+  }
+
+  onDrop(args) {
+    const [el, target, source, sibling] = args;
+    const payload = {
+      workItem: '',
+      destinationWorkItemID: '',
+      direction: 'above'
+    };
+    if (sibling === null) {
+      payload.direction = 'below';
+    } else {
+      console.log(sibling.children[0].getAttribute('data-id'), '####-37');
+    }
+    console.log(el.children[0].getAttribute('data-id'), '####-34');
+    console.log(target, '####-35');
+    console.log(source, '####-36');
+  }
 }
--- src/app/components_ngrx/planner-board-column/planner-board-column.module.ts
+++ src/app/components_ngrx/planner-board-column/planner-board-column.module.ts
@@ -1,14 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { DragulaModule, DragulaService } from 'ng2-dragula';
 import { PlannerCardModule } from './../planner-card/planner-card.module';
 import { PlannerBoardColumnComponent } from './planner-board-column.component';
-
 @NgModule({
   imports: [
     CommonModule,
+    DragulaModule,
     PlannerCardModule
   ],
   declarations: [ PlannerBoardColumnComponent ],
+  providers: [ DragulaService ],
   exports: [ PlannerBoardColumnComponent ]
 })
 
